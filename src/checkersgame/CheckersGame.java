8be0181bcb2b93e388adfb4feac18c6b71080035
shabhshah <rishabhshah28@gmail.com>
2016-04-27 08:37:03 -0400

    the UI now runs

--- src/checkersgame/CheckersGame.java
+++ src/checkersgame/CheckersGame.java
@@ -15,7 +15,7 @@ public class CheckersGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CheckersUI.run();
     }
     
 }
